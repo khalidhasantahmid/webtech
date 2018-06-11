e949d5ce83d7fd945ba9fddcdfb6bd4ce19f262c
khalidhasantahmid <39687304+khalidhasantahmid@users.noreply.github.com>
2018-06-11 10:33:43 +0600

    Add files via upload

---
+++ lab_11-6-18/login_handler.php
@@ -0,0 +1,14 @@
+<?php
+
+	$un=$_GET['un'];
+	$pa=$_GET['pass'];
+
+
+	echo "<html>";
+		echo "<body>";
+			echo "<h1>Name : ".$un."</h1>";
+			echo "<h1>Pass : ".$pa."</h1>";		
+		echo "</body>";
+	echo "</html>";
+	
+?>
